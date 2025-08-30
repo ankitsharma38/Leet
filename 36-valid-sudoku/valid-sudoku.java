96eb7d33612e17b0154d5909705d9c37df83e36c
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-08-30 23:39:20 +0530

    Time: 13 ms (20.67%) | Memory: 45.1 MB (11.32%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // here we can use hash to trac we have seen
+        HashSet<String> seen = new HashSet<>();
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.') continue;
+                
+                // Create keys for row, column, and box
+                String rowKey = num + " in row " + i;
+                String colKey = num + " in col " + j;
+                String boxKey = num + " in box " + (i / 3) + "-" + (j / 3);
+                
+                // If already seen, invalid
+                if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

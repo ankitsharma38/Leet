ab16cd432dabd51fb3b3a8af0294012e620560b2
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-08-31 00:05:43 +0530

    Time: 3 ms (91.59%) | Memory: 45.1 MB (6.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
 
         char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
@@ -8,6 +8,7 @@ class Solution {
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        return Arrays.equals(arr1, arr2);
+        return Arrays.equals(arr1,arr2);
+
     }
 }

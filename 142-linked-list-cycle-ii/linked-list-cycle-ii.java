da89ac30480278f8e573cccb93712e6196dec894
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-09-04 20:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (84.11%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -19,6 +19,6 @@ public class Solution {
                 return slow; // start of cycle
             }
         }
-        return null; // no cycle
+        return null; //if no cycle then returning null 
     }
 }

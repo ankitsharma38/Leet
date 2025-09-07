303d09c40a05b8a3212517c26f01bdca7f80830a
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-09-08 02:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (75.87%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,45 +1,45 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-
-        // Step 1: Find the break point
-        int ind = -1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i;
+        int ind1=-1;
+        int ind2=-1;
+        // step 1 find breaking point 
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind1=i;
                 break;
             }
         }
-
-        // If no break point -> last permutation, reverse the array
-        if (ind == -1) {
-            reverse(nums, 0, n - 1);
-            return;
+        // if there is no breaking  point 
+        if(ind1==-1){
+            reverse(nums,0);
         }
-
-        // Step 2: Find the next greater element and swap
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums, i, ind);
-                break;
+        
+        else{
+            // step 2 find next greater element and swap with ind2
+            for(int i=nums.length-1;i>=0;i--){
+                if(nums[i]>nums[ind1]){
+                    ind2=i;
+                    break;
+                }
             }
+
+            swap(nums,ind1,ind2);
+            // step 3 reverse the rest right half
+            reverse(nums,ind1+1);
         }
-
-        // Step 3: Reverse the suffix
-        reverse(nums, ind + 1, n - 1);
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+    void reverse(int[] nums,int start){
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
         }
     }
 }

c4d6515df60cdf7608aed27eb85c1db7d9b8c495
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-09-08 02:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (37.54%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Find the break point
+        int ind = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        // If no break point -> last permutation, reverse the array
+        if (ind == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 2: Find the next greater element and swap
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums, ind + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}

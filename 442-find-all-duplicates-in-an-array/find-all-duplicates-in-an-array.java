ef54a5dae02f5d41d17118aca0d14ffcc7c5bde3
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-10-14 23:04:51 +0530

    Time: 22 ms (23.80%) | Memory: 57.5 MB (24.07%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,21 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+        // List<Integer> resultSet = new ArrayList<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     // Get the index that this number corresponds to
+        //     int index = Math.abs(nums[i]) - 1;
+
+        //     // If the number at this index is already negative, we found a duplicate
+        //     if (nums[index] < 0) {
+        //         resultSet.add(index + 1);
+        //     }
+
+        //     // Flip the number at this index to negative to mark it as seen
+        //     nums[index] = -nums[index];
+        // }
+
+        // return resultSet;
+//     }
+// }
+
 
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
+
         List<Integer> resultSet = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            // Get the index that this number corresponds to
-            int index = Math.abs(nums[i]) - 1;
+        // Set to store unique numbers
+        Set<Integer> uniqueSet = new HashSet<>();
+        for (int num : nums) {
 
-            // If the number at this index is already negative, we found a duplicate
-            if (nums[index] < 0) {
-                resultSet.add(index + 1);
-            }
+        // If already present, then it is a duplicate
+        if (uniqueSet.contains(num))
+            resultSet.add(num);
 
-            // Flip the number at this index to negative to mark it as seen
-            nums[index] = -nums[index];
+        // Add the number to the set
+        uniqueSet.add(num);
         }
 
         return resultSet;

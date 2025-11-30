9459a94b556f88c0c5f3f6c86779ac12e5d1a6cf
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-11-30 23:43:23 +0530

    Time: 477 ms (31.54%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,9 +1,13 @@
 # Write your MySQL query statement below
-select id,
-case when p_id is null then 'Root'
-when Id in (select distinct p_id from Tree) then 'Inner'
-else
-'Leaf'
-end as type
-from Tree
-order by id
+SELECT 
+    t.id,
+    CASE
+        WHEN t.p_id IS NULL THEN 'Root'
+        WHEN c.p_id IS NULL THEN 'Leaf'
+        ELSE 'Inner'
+    END AS type
+FROM Tree t
+LEFT JOIN Tree c
+    ON t.id = c.p_id
+GROUP BY t.id, t.p_id
+ORDER BY t.id;

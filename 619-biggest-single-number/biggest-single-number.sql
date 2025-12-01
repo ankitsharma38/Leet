ce8e3754ca8ee3a2e91e7c848aaeed20377dc492
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-12-01 20:14:38 +0530

    Time: 400 ms (57.64%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,12 +1,4 @@
 # Write your MySQL query statement below
--- SELECT MAX(num) AS num
--- FROM (
---     SELECT num
---     FROM MyNumbers
---     GROUP BY num
---     HAVING COUNT(*) = 1
--- ) AS singles;
-
 select Max(num) As num
 from (
     select num 

b7c934001b24af7f481dff79f05e21871d06ca83
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2025-12-23 23:24:00 +0530

    Time: 496 ms (48.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select 
+p.project_id,
+round(avg(cast(experience_years as decimal(20,2))),2) as average_years
+from project p
+join employee e
+on p.employee_id=e.employee_id
+group by project_id

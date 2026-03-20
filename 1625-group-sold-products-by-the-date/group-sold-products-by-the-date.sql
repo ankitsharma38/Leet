a44298edbe6120680e5f19e68fce8f9df45105e3
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2026-03-20 11:56:28 +0530

    Time: 480 ms (23.84%) | Memory: 0B (100.00%) - LeetSync

--- 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -1,11 +1,6 @@
-SELECT
-    sell_date,
-    COUNT(DISTINCT product) AS num_sold,
-    GROUP_CONCAT(
-        DISTINCT product
-        ORDER BY product
-        SEPARATOR ','
-    ) AS products
+SELECT sell_date,
+		COUNT(DISTINCT(product)) AS num_sold, 
+		GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
 FROM Activities
 GROUP BY sell_date
-ORDER BY sell_date;
+ORDER BY sell_date ASC

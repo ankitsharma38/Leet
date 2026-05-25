c22fd2bd1c43df5fa4140d1818b0eb2670078d1b
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2026-05-25 18:50:04 +0530

    Time: 428 ms (39.03%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,8 @@
-# Write your MySQL query statement below
-select Person.firstName, Person.lastName, Address.city ,Address.state 
-FROM Person
-LEFT JOIN Address
-ON Person.personId = Address.personId;
+-- SELECT p.firstName, p.lastName, a.city, a.state FROM Person p  --accessing first hench LEFT
+-- LEFT JOIN Address a USING (personId) -- accessing second hence RIGHT. but we want every row in person so LEFT JOIN.
+
+
+
+
+Select p.firstName, p.lastName, a.city, a.state FROM Person p
+LEFT JOIN Address a USING (personID);

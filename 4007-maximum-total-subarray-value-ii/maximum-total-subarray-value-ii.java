5151a98defe9b87b2629b4cd3706c2f3636dd016
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2026-06-10 16:32:52 +0530

    Time: 120 ms (78.13%) | Memory: 168.2 MB (50.00%) - LeetSync

--- 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.java
+++ 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.java
@@ -1,76 +1,45 @@
 class Solution {
 
-    int[][] mx, mn;
-    int[] lg;
-
-    long value(int l, int r) {
-        int len = r - l + 1;
-        int p = lg[len];
-
-        int maxi = Math.max(mx[p][l], mx[p][r - (1 << p) + 1]);
-        int mini = Math.min(mn[p][l], mn[p][r - (1 << p) + 1]);
-
-        return (long) maxi - mini;
-    }
-
     public long maxTotalValue(int[] nums, int k) {
         int n = nums.length;
-
-        lg = new int[n + 1];
-        for (int i = 2; i <= n; i++)
-            lg[i] = lg[i / 2] + 1;
-
-        int m = lg[n] + 1;
-
-        mx = new int[m][n];
-        mn = new int[m][n];
-
+        int logn = 32 - Integer.numberOfLeadingZeros(n);
+        int[][] stMax = new int[n][logn];
+        int[][] stMin = new int[n][logn];
         for (int i = 0; i < n; i++) {
-            mx[0][i] = nums[i];
-            mn[0][i] = nums[i];
+            stMax[i][0] = stMin[i][0] = nums[i];
         }
-
-        for (int p = 1; p < m; p++) {
-            int len = 1 << p;
-            int half = len >> 1;
-
-            for (int i = 0; i + len <= n; i++) {
-                mx[p][i] = Math.max(mx[p - 1][i],
-                                    mx[p - 1][i + half]);
-
-                mn[p][i] = Math.min(mn[p - 1][i],
-                                    mn[p - 1][i + half]);
+        for (int j = 1; j < logn; j++) {
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                stMax[i][j] = Math.max(
+                    stMax[i][j - 1],
+                    stMax[i + (1 << (j - 1))][j - 1]
+                );
+                stMin[i][j] = Math.min(
+                    stMin[i][j - 1],
+                    stMin[i + (1 << (j - 1))][j - 1]
+                );
             }
         }
-
-        PriorityQueue<long[]> pq =
-            new PriorityQueue<>((a, b) ->
-                Long.compare(b[0], a[0]));
-
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         for (int l = 0; l < n; l++) {
-            pq.offer(new long[]{
-                value(l, n - 1), l, n - 1
-            });
+            int j = 31 - Integer.numberOfLeadingZeros(n - 1 - l + 1);
+            int mx = Math.max(stMax[l][j], stMax[n - 1 - (1 << j) + 1][j]);
+            int mn = Math.min(stMin[l][j], stMin[n - 1 - (1 << j) + 1][j]);
+            pq.offer(new int[] { mx - mn, l, n - 1 });
         }
-
         long ans = 0;
-
         while (k-- > 0) {
-            long[] cur = pq.poll();
-
-            long val = cur[0];
-            int l = (int) cur[1];
-            int r = (int) cur[2];
-
-            ans += val;
-
+            int[] top = pq.poll();
+            ans += top[0];
+            int l = top[1];
+            int r = top[2];
             if (r > l) {
-                pq.offer(new long[]{
-                    value(l, r - 1), l, r - 1
-                });
+                int j = 31 - Integer.numberOfLeadingZeros(r - 1 - l + 1);
+                int mx = Math.max(stMax[l][j], stMax[r - 1 - (1 << j) + 1][j]);
+                int mn = Math.min(stMin[l][j], stMin[r - 1 - (1 << j) + 1][j]);
+                pq.offer(new int[] { mx - mn, l, r - 1 });
             }
         }
-
         return ans;
     }
 }
